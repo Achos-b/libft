f280a67afbbf8b8fae5d73f3ab9b12a7ab935639
Badr Kaztaoui <bkaztaou@e2r4p6.1337.ma>
2022-11-05 05:21:55 +0100

    Finish the Libft project without bonus

--- Makefile
+++ Makefile
@@ -6,23 +6,33 @@
 #    By: bkaztaou <bkaztaou@student.1337.ma>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/11/04 17:58:02 by bkaztaou          #+#    #+#              #
-#    Updated: 2022/11/04 19:04:05 by bkaztaou         ###   ########.fr        #
+#    Updated: 2022/11/05 05:11:28 by bkaztaou         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-CC=cc
-CFLAGS=-Wall -Wextra -Werror
-SRC=$(wildcard *.c)
-OBJ=$(patsubst %.c, %.o, $(SRC))
-LIB=libft.a
+NAME = libft.a
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
 
-all:$(LIB)
+FILES = ft_isalnum.c ft_isprint.c ft_memcpy.c ft_putendl_fd.c ft_strchr.c ft_strlcat.c ft_strncmp.c ft_substr.c \
+	ft_atoi.c ft_isalpha.c ft_itoa.c ft_memmove.c ft_putnbr_fd.c ft_strdup.c ft_strlcpy.c ft_strnstr.c ft_tolower.c \
+	ft_bzero.c ft_isascii.c ft_memchr.c ft_memset.c ft_putstr_fd.c ft_striteri.c ft_strlen.c ft_strrchr.c  ft_toupper.c \
+	ft_calloc.c ft_isdigit.c ft_memcmp.c ft_putchar_fd.c ft_split.c ft_strjoin.c ft_strmapi.c ft_strtrim.c
 
-$(LIB):$(OBJ)
-	ar -csr $(LIB) *.o
+OBJ = $(FILES:.c=.o)
 
-%.o:%.c
-	$(CC) $(CFLAGS) -c $^ -o $@
+all : $(NAME)
+
+$(NAME) : $(OBJ)
+	ar rc $(NAME) $(OBJ)
+
+$(OBJ) : $(FILES)
+	$(CC) $(CFLAGS) -c $(FILES)
 
 clean:
-	rm -rf *.o $(LIB)
+	rm -rf $(OBJ)
+
+fclean: clean
+	rm -rf $(NAME)
+
+re: fclean all
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bkaztaou <bkaztaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 17:58:06 by bkaztaou          #+#    #+#             */
-/*   Updated: 2022/11/04 23:09:21 by bkaztaou         ###   ########.fr       */
+/*   Updated: 2022/11/05 05:20:50 by bkaztaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_atoi(const char *str);
+int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
@@ -28,6 +30,7 @@ int		ft_toupper(int c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *str);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
@@ -42,8 +45,11 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strrchr(const char s[], int c);
+char	**ft_split(char const *s, char c);
 char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
 char	*ft_itoa(int n);
 
 #endif
