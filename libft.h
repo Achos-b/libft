6940d8142b1143d2d00927a823fff7061b151d56
Badr Kaztaoui <bkaztaou@e2r4p6.1337.ma>
2022-11-05 01:00:36 +0100

    Finish the First part (libft functions)

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bkaztaou <bkaztaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 17:58:06 by bkaztaou          #+#    #+#             */
-/*   Updated: 2022/11/04 19:24:40 by bkaztaou         ###   ########.fr       */
+/*   Updated: 2022/11/04 23:09:21 by bkaztaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@
 # include <stdlib.h>
 # include <limits.h>
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_isdigit(int c);
-int	ft_tolower(int c);
-int	ft_toupper(int c);
-size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_isdigit(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *str);
 void	*ft_memmove(void *dst, const void *src, size_t len);
